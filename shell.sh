e1ed46babedbe561a0a69b3ceb1ef30e274c530a
aruntony005 <64554015+aruntony005@users.noreply.github.com>
2020-08-07 10:36:01 -0700

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,17 +1,10 @@
-docker build -t my-demo web-demo/
-
-sudo docker save -o /root/demo-image.tar `docker images --no-trunc | grep -i my-demo | awk '{print $3}'`
-
+sudo docker build -t my-demo web-demo/
+sudo docker save -o /root/demo-image.tar `docker images --no-trunc | grep -i my-demo | awk '{print \$3}'`
 sudo scp -i /root/.ssh/id_rsa /root/demo-image.tar root@worker:/root/
-
-ssh worker "sudo docker load -i /root/demo-image.tar"
-
-image_id=`docker images --no-trunc | grep -i my-demo | awk '{print $3}'`
-
-sed "s/WEB-IMAGE/$image_id/g" web-demo/web-deploy.yaml_template > web-demo/web-deploy.yaml
-
-kubectl delete deployment.apps/web-demo
-kubectl delete service/web-demo
-
-kubectl create -f demo/web-deploy.yaml
-kubectl create -f demo/web-service.yaml
+sudo ssh root@worker "sudo docker load -i /root/demo-image.tar"
+image_id=`sudo docker images --no-trunc | grep -i my-demo | awk '{print $3}'`
+sudo sed "s/WEB-IMAGE/$image_id/g" web-demo/web-deploy.yaml_template > web-demo/web-deploy.yaml
+sudo kubectl delete deployment.apps/web-demo
+sudo kubectl delete service/web-demo
+sudo kubectl create -f web-demo/web-deploy.yaml
+sudo kubectl create -f web-demo/web-service.yaml
